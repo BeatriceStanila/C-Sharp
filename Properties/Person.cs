54cde8e3bd4fab132274ca4e26592d6c97d27469
btxoana <beatrice.stanila@gmail.com>
2024-03-06 16:10:10 +0000

    Add project files.

---
+++ Properties/Person.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Properties
+{
+    public class Person
+    {
+
+        //1. Properties shortcut prop + tab 
+        //2. Constructors shortcut ctor + tab
+
+
+        //auto-implemented properties
+        //public string Name { get; set; }
+        //public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }     // private set - it's read only (the birthdate can't be changed) and it's set in the constructor above
+
+        //constructor to set the date
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+
+        public int Age
+        {
+        // a property that calculates the person's age based on the value of another property
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+
+        }
+    }
+}
+
+// make sure that classes are always in a valid state 
---
+++ Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Properties
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person (new DateTime(1989, 12, 13));
+            
+            Console.WriteLine(person.Age);
+        }
+    }
+}
